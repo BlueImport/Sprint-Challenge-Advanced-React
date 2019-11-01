d17b3c43e11bdc7700fc6bfcb444721755f8311c
Kris <kristian.claire.101@gmail.com>
2019-11-01 09:27:47 -0700

    adds data to app

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+afterEach(rtl.cleanup);
+
+test('Render the app', () => {
+  const wrapper = rtl.render(<App />)
+  const element = wrapper.getByText(/women's world cup/i)
+  wrapper.debug(wrapper.queryByText(/united states/i))
+  expect(element).toBeVisible()
+})
+
+test('Render count input', () => {
+  const wrapper =  rtl.render(<App />)
+  const element = wrapper.getByPlaceHolderText(/count/i)
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
